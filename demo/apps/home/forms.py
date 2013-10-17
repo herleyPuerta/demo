6dd41efdbefb4f7b55c1f75b91864e8f71ddd159
Herley Puerta <hypuerta@gmail.com>
2013-10-17 09:14:35 -0500

    conflictos

--- demo/apps/home/forms.py
+++ demo/apps/home/forms.py
@@ -8,4 +8,4 @@ class ContactForm(forms.Form):
 class LoginForm(forms.Form):
 	username = forms.CharField(widget=forms.TextInput())
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
-
+	
