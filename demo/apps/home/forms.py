4c79ed1da96edadbf8db2179859ea798e3d69361
Herley Puerta <hypuerta@hotmail.com>
2013-10-17 08:10:21 -0500

    registro de usuarios

--- demo/apps/home/forms.py
+++ demo/apps/home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
 	Email	= forms.EmailField(widget=forms.TextInput())
@@ -9,3 +10,32 @@ class LoginForm(forms.Form):
 	username = forms.CharField(widget=forms.TextInput())
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False))
 
+class RegisterForm(forms.Form):
+	username		= forms.CharField(widget=forms.TextInput(),label='Nombre de Usuario')
+	email    		= forms.EmailField(label="Correo Electronico",widget=forms.TextInput())
+	password_one 	= forms.CharField(widget=forms.PasswordInput(render_value=False),label='Contrasena')
+	password_two 	= forms.CharField(widget=forms.PasswordInput(render_value=False),label='Confirmar Contrasena')
+
+	def clean_username(self):
+		username 	= self.cleaned_data['username']
+		try:
+			u = User.objects.get(username=username)
+		except User.DoesNotExist:
+			return username
+		raise forms.ValidationError('Nombre de usuario ya existe')
+
+	def clean_email(self):
+		email = self.cleaned_data['email']
+		try:
+			u = User.objects.get(email=email)
+		except User.DoesNotExist:
+			return email
+		raise forms.ValidationError('Email ya registrado')
+
+	def clean_password_two(self):
+		password_one = self.cleaned_data['password_one']
+		password_two = self.cleaned_data['password_two']
+		if password_one == password_two:
+			pass
+		else:
+			raise forms.ValidationError('password no coinciden')
